d95296f8535a6f729ea205930d25b8a41f2f6bd8
vienct3 <chautrivien2103@gmail.com>
2023-10-10 18:23:40 +0700

    fix: remove script compile, fix scripts

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#Exec make command
-make
-# Add execute permission
-chmod +x ./bin/publisher-client
-# Compress everything we need to tar.gz
-tar -czvf app.tar.gz ./bin/publisher-client
-# Transfer file
-curl --upload-file app.tar.gz https://cpek.fpt.vn/fs-tx/app.tar.gz
